a618fd242e11d029a3fe31cbdfa7d71327bd2569
Adam Sernheim <Adam.Sernheim@kvadrat.se>
2018-11-30 11:36:29 +0100

    changed some stuff in vimrc

--- install/link.sh
+++ install/link.sh
@@ -2,6 +2,10 @@
 
 DOTFILES=$HOME/.dotfiles
 
+echo "Fetching plug plugin for vim"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 echo "creating symlinks"
 linkables=$( ls -1 -d **/*.symlink )
 for file in $linkables ; do
